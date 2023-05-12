e6b76112ce94a783a7db9de5a83871f40b70f392
santisan <125523558+sntiwey@users.noreply.github.com>
2023-05-11 20:04:25 -0600

    Add files via upload

---
+++ Main Tarea 15.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        Ventana2 ventana2 = new Ventana2("Ejemplo");
+    }
+}
---
+++ ModeloTabla Juego Tarea 15.java
@@ -0,0 +1,111 @@
+import javax.swing.event.TableModelListener;
+import javax.swing.table.TableModel;
+import java.sql.Struct;
+import java.util.ArrayList;
+
+public class ModeloTablaJuego implements TableModel {
+    public static final int COLS = 6;
+    ArrayList<VideoJuego> datos;
+
+    public ModeloTablaJuego(ArrayList<VideoJuego> datos) {
+        this.datos = datos;
+    }
+
+    @Override
+    public int getRowCount() {
+        return datos.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return COLS;
+    }
+
+    @Override
+    public String getColumnName(int columnIndex) {
+        String columName = "";
+        switch (columnIndex){
+            case 0:
+                columName = "Id.";
+                break;
+            case 1:
+                columName = "Nombre.";
+                break;
+            case 2:
+                columName = "Genero.";
+                break;
+            case 3:
+                columName = "Peso en GB.";
+                break;
+            case 4:
+                columName = "Plataforma.";
+                break;
+            case 5:
+                columName = "Desarrollador.";
+                break;
+        }
+        return columName;
+    }
+
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        switch (columnIndex){
+            case 0:
+                return  Integer.class;
+                case 1:
+                return  String.class;
+                case 2:
+                return  String.class;
+                case 3:
+                return  Double.class;
+                case 4:
+                return  String.class;
+                case 5:
+                return  String.class;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        return false;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        VideoJuego tmp = datos.get(rowIndex);
+        switch (columnIndex){
+            case 0:
+                return  tmp.getId();
+            case 1:
+                return  tmp.getNombre();
+            case 2:
+                return  tmp.getGenero();
+            case 3:
+                return  tmp.getPesoEnGB();
+            case 4:
+                return  tmp.getPlataforma();
+            case 5:
+                return  tmp.getDesarrollador();
+        }
+        return null;
+    }
+
+    @Override
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+
+    }
+
+    @Override
+    public void addTableModelListener(TableModelListener l) {
+
+    }
+
+    @Override
+    public void removeTableModelListener(TableModelListener l) {
+
+    }
+    public void agregarJuego(VideoJuego juego){
+        this.datos.add(juego);
+    }
+}
